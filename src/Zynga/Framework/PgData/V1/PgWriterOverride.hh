4747f6ea81204df008d8ba3692062a1f1f3d6471
Andrew Dirksen <adirksen@zynga.com>
2019-11-07 22:33:07 +0000

    Minor Bug Fixes

--- src/Zynga/Framework/PgData/V1/PgWriterOverride.hh
+++ src/Zynga/Framework/PgData/V1/PgWriterOverride.hh
@@ -3,5 +3,15 @@
 namespace Zynga\Framework\PgData\V1;
 
 use Zynga\Framework\StorableObject\V1\Base;
+use Zynga\Framework\Type\V1\BoolBox;
 
-class PgWriterOverride extends Base {}
+class PgWriterOverride extends Base {
+
+  public BoolBox $override;
+
+  public function __construct(): void {
+    parent::__construct();
+    $this->override = new BoolBox();
+    $this->override->setDefaultValue(true);
+  }
+}
